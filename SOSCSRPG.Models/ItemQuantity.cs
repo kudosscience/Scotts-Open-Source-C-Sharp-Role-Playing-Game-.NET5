77928d600a28ad04bd8a7d63ac08a5b33415c7ba
Henry Ward <45144290+kudosscience@users.noreply.github.com>
2025-01-15 17:06:17 +0000

    Moving the Engine classes to their new projects

---
+++ SOSCSRPG.Models/ItemQuantity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOSCSRPG.Services.Factories;
+
+namespace SOSCSRPG.Models
+{
+    public class ItemQuantity
+    {
+        private readonly GameItem _gameItem;
+        public int ItemID => _gameItem.ItemTypeID;
+        public int Quantity { get; }
+        public string QuantityItemDescription =>
+            $"{Quantity} {_gameItem.Name}";
+        public ItemQuantity(GameItem item, int quantity)
+        {
+            _gameItem = item;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ SOSCSRPG.Models/QuestStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSCSRPG.Models
+{
+    public class QuestStatus: INotifyPropertyChanged
+    {
+        public Quest PlayerQuest { get; }
+        public bool IsCompleted { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public QuestStatus(Quest quest)
+        {
+            PlayerQuest = quest;
+            IsCompleted = false;
+        }
+    }
+}
